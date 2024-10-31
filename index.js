4648a49ae2fbe609796b5039f4eef7fef6279716
NataliDiB88 <natali.di.bartolo@gmail.com>
2024-10-31 16:42:07 -0400

    changes

---
+++ index.js
@@ -0,0 +1,100 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description for your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Provide contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide test instructions:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ];
+
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateReadme(answers);
+  
+    fs.writeFile('README.md', readmeContent, (err) =>
+      err ? console.error(err) : console.log('README.md generated successfully!')
+    );
+  });
+
+  function generateReadme(answers) {
+    return `
+  # ${answers.title}
+  
+  ## Description
+  ${answers.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${answers.installation}
+  
+  ## Usage
+  ${answers.usage}
+  
+  ## Contributing
+  ${answers.contributing}
+  
+  ## Tests
+  ${answers.tests}
+  
+  ## License
+  This project is licensed under the ${answers.license} license.
+  
+  ## Questions
+  For any questions, please contact me:
+  
+  - GitHub: [${answers.github}](https://github.com/${answers.github})
+  - Email: ${answers.email}
+    `;
+  }
+  
+  
+  
